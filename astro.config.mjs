3198165996fa97b13acd26b589765c9929c8dc21
Osmo Someroja <osmo@someroja.com>
2023-04-30 23:20:07 +0300

    Replace Tailwind with hand-crafted CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  integrations: [tailwind()],
   site: "https://vastavalo.github.io", // TODO: change to custom domain
   base: "/vastavalo-website",
   vite: {
